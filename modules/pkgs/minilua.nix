6f7a0f527dbc5ab1b12a9be829dde0b50ac55884
Alaestor Weissman <alaestor@0x04.cc>
2026-07-16 12:00:55 -0400

    feat(pkgs): switch to minilua fork

--- modules/pkgs/minilua.nix
+++ modules/pkgs/minilua.nix
@@ -1,40 +1,47 @@
 { ... }:
 {
-  perSystem = { pkgs, ... }: {
-    packages.minilua = pkgs.python3.pkgs.buildPythonPackage {
+  perSystem =
+    { pkgs, fetchFromCodeberg, ... }:
+    let
       pname = "minilua";
       version = "1.1.0";
+      owner = "alaestor";
+      repo = "fork-MiniLua";
+    in
+    {
+      packages.minilua = pkgs.python3.pkgs.buildPythonPackage {
+        inherit pname version;
 
-      src = pkgs.fetchFromGitHub {
-        owner = "aradooo";
-        repo = "MiniLua";
-        rev = "v1.1.0";
-        hash = "sha256-vfE2mCe4QTdwowIAoCV6e9u5Xzj1EJEfemllIq6D1Xs=";
-      };
+        src = pkgs.fetchFromCodeberg {
+          inherit owner repo;
+          rev = "v${version}";
+          hash = "sha256-vfE2mCe4QTdwowIAoCV6e9u5Xzj1EJEfemllIq6D1Xs=";
+        };
 
-      pyproject = true;
+        pyproject = true;
 
-      build-system = with pkgs.python3.pkgs; [
-        setuptools
-        wheel
-      ];
+        build-system = with pkgs.python3.pkgs; [
+          setuptools
+          wheel
+        ];
 
-      dependencies = with pkgs.python3.pkgs; [
-        click
-        colorama
-      ];
+        dependencies = with pkgs.python3.pkgs; [
+          click
+          colorama
+        ];
 
-      doInstallCheck = true;
+        doInstallCheck = true;
 
-      installCheckPhase = ''
-        $out/bin/minilua --version
-      '';
+        installCheckPhase = ''
+          $out/bin/minilua --version
+        '';
 
-      meta = with pkgs.lib; {
-        description = "MiniLua";
-        homepage = "https://github.com/aradooo/MiniLua";
-        license = licenses.mit;
+        meta = with pkgs.lib; {
+          description = "MiniLua";
+          #homepage = "https://github.com/aradooo/MiniLua"; -- vanished?
+          homepage = "https://codeberg.org/alaestor/fork-MiniLua";
+          license = licenses.mit;
+        };
       };
     };
-  };
 }
